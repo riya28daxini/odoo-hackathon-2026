5998c7b91845999e71ed551b22c03d3e5bc8b5d5
Vasavi Chaudhari <vbchaudhari.2007@gmail.com>
2026-08-22 14:52:11 +0530

    Clean authenctication backend

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,21 +1,17 @@
 import express from 'express';
+
 import {
   register,
   login,
-  verifyOtp,
-  forgotPassword,
-  resetPassword,
   getMe
 } from '../controllers/authController.js';
+
 import { requireAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/verify-otp', verifyOtp);
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password', resetPassword);
 router.get('/me', requireAuth, getMe);
 
 export default router;
